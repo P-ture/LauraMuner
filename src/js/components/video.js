1bc56d48322b8419f2f377d137a7ccf9343ef711
Wildhoney <adam.timberlake@gmail.com>
2017-05-24 19:33:32 +0100

    Added resetting of video onEnded

--- src/js/components/video.js
+++ src/js/components/video.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+
 /**
  * @class Carousel
  * @extends {PureComponent}
  */
-
-
 export default class Video extends Component {
 
     /**
-     * @constant state
-     * @type {Object}
+     * @constructor
+     * @return {Video}
      */
     constructor() {
         super();
@@ -19,23 +18,42 @@ export default class Video extends Component {
         };
     }
 
+    /**
+     * @method play
+     * @return {void}
+     */
     play() {
         const video = document.querySelector('video');
-        this.setState({play: !this.state.play});
+        this.setState({ play: !this.state.play });
         return video.play()
     };
+
+    /**
+     * @method pause
+     * @return {void}
+     */
     pause() {
         const video = document.querySelector('video');
-        this.setState({play: !this.state.play});
+        this.setState({ play: !this.state.play });
         return video.pause()
     };
+
+    /**
+     * @method reset
+     * @return {void}
+     */
+    reset() {
+        const video = document.querySelector('video');
+        video.currentTime = 0;
+        video.pause();
+        this.setState({ play: !this.state.play });
+    }
 
     render() {
         return (
             <section className="video">
                 {this.state.play ? '' : <div className="playBtn" />}
-                <video src={this.props.src} onClick={this.state.play ? () => this.pause() : () => this.play()} />
-
+                <video src={this.props.src} onClick={() => this.state.play ? this.pause() : this.play()} onEnded={() => this.reset()} />
             </section>
         )
 
